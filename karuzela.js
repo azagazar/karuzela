beead0613c3e797c89cc276782dd09985b87b1bd
Agata Zarod <Agata Zarod>
2017-04-10 16:16:20 +0200

    aktualizacja plikow

---
+++ karuzela.js
@@ -0,0 +1,24 @@
+$(function(){
+        var carousel = $('.carousel ul');
+        var carouselList = $('li')
+        
+        function changeSlide () {
+        	
+        }
+
+        setInterval(changeSlide, 3000);
+        
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+
+		function moveFirstSlide () {
+
+			var firstItem = carouselList.find("li:first");
+			var lastItem = carouselList.find("li:last");
+
+			lastItem.after(firstItem)
+
+			carouselList.css({marginLeft:0});
+		}
+
+			
+	});
